f7614a8afd426374819677730716e9add41d9313
Rimou <rimerai16@hotmail.com>
2014-03-21 21:43:06 +0100

    modificaciones alumno1 x2

--- proyectoGIT/GitAux.java
+++ proyectoGIT/GitAux.java
@@ -12,6 +12,8 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+        System.out.println("modificado por alumno 1"); // alumno 1
+        
        
     }
     
@@ -21,6 +23,8 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+        // modificaciones alumno 1
+        System.out.println("Alumno 1 inserta esta sentencia");
         
     }
    
